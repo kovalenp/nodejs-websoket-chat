6e00d2b18bdd1d8d3c3257841cc9acfd68a74700
Pavel Kovalenko <pavel@kovalenko.cz>
2018-11-19 15:35:34 +0100

    Add initial messaging support

--- server/server.js
+++ server/server.js
@@ -1,11 +1,46 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 const { name } = require('../package.json');
 
 
-const PUBLIC_PATH = path.join(__dirname, '../public')
+const PUBLIC_PATH = path.join(__dirname, '../public');
 const PORT = process.env.PORT || 3000;
 
-const app = express()
-app.use(express.static(PUBLIC_PATH))
-app.listen(PORT, () => console.log(` ${name} app listening on port ${PORT}!`))
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newMessage', 
+  {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+  });
+
+  socket.broadcast.emit('newMessage', 
+  {
+    from: 'Admin',
+    text: 'New user has joined',
+    createdAt: Date.now(),
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log(JSON.stringify(message));
+    io.emit('newMessage',
+      { from: message.from,
+        text: message.text,
+        createdAt: Date.now(),
+      })
+  })
+
+  socket.on('disconnect', (socket) => {
+    console.log('User disconnected');
+  })
+});
+
+app.use(express.static(PUBLIC_PATH));
+server.listen(PORT, () => console.log(` ${name} app listening on port ${PORT}!`));
