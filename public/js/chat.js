f6565315bdeaa99641a21a5e0c5e30f48b094b4f
Pavel Kovalenko <pavel@kovalenko.cz>
2018-11-25 11:01:29 +0100

    Add user list

--- public/js/chat.js
+++ public/js/chat.js
@@ -36,6 +36,15 @@ socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
 
+socket.on('updateUserList', function(users){
+  const ul = jQuery('<ul></ul>')
+  users.forEach(function(user) {
+    ul.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ul);
+});
+
 socket.on('newMessage', function(message) {
   const template = jQuery('#message-template').html();
   const html = Mustache.render(template, {
--- server/server.js
+++ server/server.js
@@ -21,18 +21,18 @@ io.on('connection', (socket) => {
   socket.on('join', (params, callback) => {
     if (!isRealString(params.name) ||
         !isRealString(params.room)) {
-      callback('Name and room name are mandatory.');
+      return callback('Name and room name are mandatory.');
     }
 
+    socket.join(params.room);
+    users.removeUser(socket.id); // remove user from previous chats
     users.addUser(socket.id, params.name, params.room);
 
-    socket.join(params.room);
-
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
     socket.broadcast
       .to(params.room)
       .emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
-
     callback();
 
   });
@@ -49,7 +49,11 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    const user = users.removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));      
+    }
   });
 });
 
